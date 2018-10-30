5cd696b219dff2cee62e2bf95310063719bc622b
wjcooper91 <wjcooper91@gmail.com>
2018-10-29 22:06:11 -0400

    edit mongo connect

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 module.exports = {
     // DB: 'mongodb://localhost:27017/auth'
-    DB: 'mongodb://<User>:<poolparty1>@ds245523.mlab.com:45523/p00l-party'
+    DB: 'mongodb://User:poolparty1@ds245523.mlab.com:45523/p00l-party'
 }
